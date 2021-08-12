d8b533e1db71e5f1ac21236eb8abae3ecee8e97c
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2021-08-12 14:36:09 +0200

    affichage produits

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 //INDEX
+
+//appel de l'API
+fetch("http://localhost:3000/api/furniture")
+//conversion en format json
+.then(data => data.json())
+.catch((error) => {
+    console.log(error);
+    // cas d'erreur, site non disponible
+    document.querySelector(".div__disponible").innerHTML += `<div class="text-center ">
+                                                                <h3>Site indisponible pour le moment</h3>
+                                                            </div>
+                                                            `;
+})
+
+.then( jsonListFurniture => {
+    for ( let jsonFurniture of  jsonListFurniture ){
+        document.querySelector(".div__produit").innerHTML += 
+        `<div class="product ">
+            <div class="card article">
+                <img src="${jsonFurniture.imageUrl}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title ">${jsonFurniture.name}</h5>
+                    <p class="card-text">${jsonFurniture.price/100 + ',00 €'}</p>
+                </div>
+            </div>
+        </div>`;
+
+        
+    };
+})
+.catch((error) => {
+    console.log(error)
+})
