dfc9b4d182bb3b306ad9f5b7db8ea482edcff119
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2021-08-25 15:16:45 +0200

    update pages

--- js/produit.js
+++ js/produit.js
@@ -1,1 +1,34 @@
-//PRODUIT
+//PANIER
+
+
+const searchParams = new URLSearchParams(window.location.search);
+const id = searchParams.get('id')
+
+//appel api avec id
+fetch(`http://localhost:3000/api/furniture/${id}`)
+.then( data => data.json() )
+.catch( (error) => {
+    console.log(error);
+    document.querySelector(".div__disponible").innerHTML += `<div class="text-center ">
+                                                                <h3> <img src="../images/Dino.png" alt="Image dino" height="50px">  Site indisponible </h3>
+                                                            </div>
+                                                            `;
+})
+
+.then ( jsonBasket => {
+    document.querySelector(".div__produit").innerHTML +=
+    `<div class="card article ">
+        <img src="${jsonBasket.imageUrl}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title ">${jsonBasket.name}</h5>
+            <p class="card-text">${jsonBasket.price/100 + ',00 €'}</p>
+        </div>
+    </div>
+    <div class="py-2">
+        <a href="#" class="btn btn-outline-primary" role="button" >Ajouter au panier</a>
+    </div>
+    `;
+})
+.catch( (error) => {
+    console.log(error)
+})
